fb57f81e7e35390f3947e7dae5cf1e9964968856
Bryan Mytko <bryan@bluefountainmedia.com>
2014-01-01 22:43:58 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cubetimes::Application.config.secret_key_base = '340a9e3815acc695d69221accb60d177f59a9b04dfab5acdd7cbd0007d50ce5de629a91fb13bea63707a2b3f775a8ca86fdf2a57b80c21fd791dd3e5d0b2ba09'
