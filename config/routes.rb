5d701b3538e795a553381d1ad1649c2defc40ae3
bryanmytko <bryanmytko@gmail.com>
2014-01-06 10:52:25 -0500

    new pages, controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Cubetimes::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'timer#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +12,7 @@ Cubetimes::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :sessions
+  resources :timer, :statistics, :profile, :sessions, :home
 
   # Example resource route with options:
   #   resources :products do
