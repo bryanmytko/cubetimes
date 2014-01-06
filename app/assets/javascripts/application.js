6088deea8bd004439f0816c4d6879d0a1bb30c98
Bryan Mytko <bryanmytko@gmail.com>
2014-01-06 10:52:25 -0500

    new pages, controllers added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,66 @@
 
 
 $(function() {
-   $('.notice,.alert').delay(500).fadeIn('normal', function() {
-      $(this).delay(1500).fadeOut();
-      $('.notice,.alert').hover().fadeOut();  
-   });
+  
+/* Tab controls */
+$('.notice,.alert').delay(500).fadeIn('normal', function() {
+  $(this).delay(1500).fadeOut();
+  $('.notice,.alert').hover().fadeOut();
+});
+
+/* Timer */
+var running = false,
+start = null,
+control = null;
+
+$('#timer_button').click(function(){
+  running = (running) ? false : true;
+  start = new Date().getTime();
+  if(running) timer();
+  else $timeout.cancel(control);
+})
+
+function start_timer(){
+  console.log('timer started...')
+}
+
+function end_timer(){
+  console.log('timer stopped...')
+}
+
+time = {
+  s: 0.00,
+  ms: 0.00
+}
+
+console.log(time.s)
+console.log(time.ms)
+
+var timer = function(){
+   var t = new Date().getTime() - start;
+   var elapsed = Math.floor(t / 10) / 100;
+   if(Math.round(elapsed) == elapsed) elapsed += '.0';
+   time.s = elapsed;
+   console.log(time.s)
+   timer();
+}
+
+/*$scope.switch = function(){
+angular.element('body').removeClass('ready');
+ start = new Date().getTime();
+ if(!running){
+   console.log('starting!');
+   running = true;
+   timer();
+ } 
+ else{
+   console.log('ended.');
+   $timeout.cancel(control);
+   running = false;
+ }
+}*/
+   
+   
+   
+   
 });
---
+++ app/controllers/statistics_controller.rb
@@ -0,0 +1,19 @@
+class StatisticsController < ApplicationController
+  include ParserConcern
+  
+  def index
+    @timesArray ||= Array.new
+    @session = Session.new  
+    times_path = File.join Rails.root, 'public', 'times'
+    if File.directory?(times_path)
+      @timesArray = parse_dir times_path
+    end
+    if !@timesArray.any?
+      @best_time = '--'
+      @best_average = '--'
+    end
+    @best_time = @@best_time
+    @best_average = @@best_average  
+  end
+  
+end
