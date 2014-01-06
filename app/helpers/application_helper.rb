1cc8ee9a6c907912e2d445cb7a93b4881a972ed1
Bryan Mytko <bryan@bluefountainmedia.com>
2014-01-06 10:52:25 -0500

    new pages, controllers added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def is_active path
+    current_path = request.fullpath.split(/\//)
+    if path == '/' && current_path[1].nil? then return 'class=active' end #root url
+    if current_path == '/' && request.fullpath == '/' then return 'class=active' end
+    return 'class=active' unless current_path[1] != path
+  end
 end
---
+++ test/helpers/timer_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TimerHelperTest < ActionView::TestCase
+end
