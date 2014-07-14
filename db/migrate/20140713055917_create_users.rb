83e43382650c351d8f6d1629d556d4b7ca78ae3b
Bryan <Mytko@Bryans-iMac.local>
2014-07-14 01:02:16 -0400

    update User model to have unique email

--- db/migrate/20140713055917_create_users.rb
+++ db/migrate/20140713055917_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :user_name
-      t.string :email
+      t.string :user_name, unique: true
+      t.string :email, :unique => true
       t.string :password_hash
       t.string :password_salt
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713055917) do
+ActiveRecord::Schema.define(version: 20140714011633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,5 +30,7 @@ ActiveRecord::Schema.define(version: 20140713055917) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
