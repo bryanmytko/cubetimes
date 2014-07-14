c0e5738323880fe1b0a69d3e1680c03466e8b721
Bryan <Mytko@Bryans-iMac.local>
2014-07-14 01:03:34 -0400

    add authentication & tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ActiveRecord::Base
   end
 
   def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
   end
 end
