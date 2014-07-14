ccb2af469a618feaa862bafcbf14513c63483328
bryanmytko <bryanmytko@gmail.com>
2014-07-14 01:02:16 -0400

    update User model to have unique email

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(user_name: 'bryan', password: 'password', password_salt: 'apple', email: 'bryan')
