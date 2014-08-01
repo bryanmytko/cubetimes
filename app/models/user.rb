757890201748de1e526e9bdeda2868384522fda1
bryanmytko <bryanmytko@gmail.com>
2014-08-01 18:28:35 -0400

    Adds User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   attr_accessor :password
   before_save :encrypt_password
 
+  validates :user_name, :email, :password, presence: true
+  validates :email, uniqueness: true
+
   def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:user) { FactoryGirl.create(:user) }
+  specify { expect(user).to be_valid }
+
+  it 'should have a user name' do
+    user.user_name = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'should have an email' do
+    user.email = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'should have a password' do
+    user.password = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'should not save the password as plain text' do
+    expect(user.password_hash).not_to eq user.password
+  end
+
+  describe 'when email is already taken' do
+    it 'should not allow duplicate emails' do
+      duplicate_user = user.dup
+      expect(duplicate_user).to_not be_valid
+    end
+  end
 end
