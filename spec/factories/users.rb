0238be11ef8f09542e407cd683db0b38b75adbc0
bryanmytko <bryanmytko@gmail.com>
2014-08-02 14:37:03 -0400

    Updates User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     user_name Faker::Name.first_name
     password Faker::Internet.password(8)
+    password_salt Faker::Lorem.word
     email Faker::Internet.email
   end
 end
