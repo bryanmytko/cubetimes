19bf7e110b4928ec9796181658948f4e5d65787d
Bryan Mytko <bryanmytko@gmail.com>
2014-12-30 18:56:49 -0500

    Adds Cubing Sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Cubetimes::Application.routes.draw do
 
   root 'timer#index'
 
-  resources :timer, only: [:index]
-  resources :statistics, only: [:index]
-  resources :profile, only: [:index]
-  resources :sessions, only: [:index]
+  resources :timer,           only: [:index]
+  resources :statistics,      only: [:index]
+  resources :profile,         only: [:index]
+  resources :cubing_sessions, only: [:create]
 end
