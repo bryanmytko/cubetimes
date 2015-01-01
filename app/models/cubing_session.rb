9e9014f1e9a990655b3e879a3bbcdd725bf46d36
Bryan Mytko <bryanmytko@gmail.com>
2014-12-31 20:18:02 -0500

    Add user.cubing_session relation

--- app/models/cubing_session.rb
+++ app/models/cubing_session.rb
@@ -1,3 +1,13 @@
 class CubingSession < ActiveRecord::Base
+  include JnetImport
+
+  belongs_to :user
+
   validates :times, length: { is: 12 }
+
+  def self.jnet_import(params)
+    CubingSession.create(
+      times: JnetImport::extract_times(params)
+    )
+  end
 end
