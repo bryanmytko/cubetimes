075b6e009d908a076c8ef9b1896ac450814f0869
bryanmytko <bryanmytko@gmail.com>
2015-01-02 03:12:13 -0500

    Add cubing session data to StatisticsController#index

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,18 +4,6 @@ class StatisticsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    # OOF. Refactor!
-    # @timesArray ||= Array.new
-    # times_path = File.join Rails.root, 'public', 'times'
-
-    # if File.directory?(times_path)
-    #   @timesArray = parse_dir times_path
-    # end
-    # if !@timesArray.any?
-    #   @best_time = '--'
-    #   @best_average = '--'
-    # end
-    # @best_time = @@best_time
-    # @best_average = @@best_average
+    @cubing_sessions = CubingSession.where(user_id: current_user.id)
   end
 end
