7b1165c531a42cac4b93bbb0e2c615f3a2b92a3c
Bryan Mytko <bryanmytko@gmail.com>
2015-01-01 22:40:19 -0500

    Cleanup style (quotes)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe User do
   let(:user) { FactoryGirl.create(:user) }
   specify { expect(user).to be_valid }
 
-  it 'should have an email' do
-    user.email = ''
+  it "should have an email" do
+    user.email = ""
     expect(user).to_not be_valid
   end
 
-  it 'should have a password' do
-    user.password = ''
+  it "should have a password" do
+    user.password = ""
     expect(user).to_not be_valid
   end
 
-  describe 'when email is already taken' do
-    it 'should not allow duplicate emails' do
+  describe "when email is already taken" do
+    it "should not allow duplicate emails" do
       duplicate_user = user.dup
       expect(duplicate_user).to_not be_valid
     end
