4faad10c7e9227560c4278a964114b82086fa187
bryanmytko <bryanmytko@gmail.com>
2015-01-05 23:03:52 -0500

    Add Profile factory

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :profile do
-    first_name "Foo"
-    last_name "MyString"
-    gender "MyString"
-    age 1
-    country "MyString"
-    favorite_puzzles "MyString"
-    fastest_3x3 "MyString"
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    gender     { %w(male female).sample }
+    age        { Faker::Number.digit }
+    country    { Faker::Address.country }
+    favorite_puzzles { %w(3x3 4x4 5x5 pyraminx square-1 magic).sample }
+    fastest_3x3 { Faker::Commerce.price }
   end
 
 end
