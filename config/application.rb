68da3dd4b80a51632f1253919d994a342f69b218
bryanmytko <bryanmytko@gmail.com>
2015-10-27 20:37:23 -0400

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,9 @@ Bundler.require(:default, Rails.env)
 
 module Cubetimes
   class Application < Rails::Application
+    config.serve_static_assets = true
+
     #config.autoload_paths += %W(#{Rails.root}/app/controllers/concerns) # in Rails4 this is automatic
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
