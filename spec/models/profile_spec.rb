07641adf8f18f0acc247256cb961d4402d5cf7c0
bryanmytko <bryanmytko@gmail.com>
2015-10-28 21:05:24 -0400

    Add Profile validations and model spec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Profile, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Profile, type: :model do
+  it "is invalid without a first name" do
+    invalid_profile = FactoryGirl.build(:profile, first_name: nil)
+    expect(invalid_profile).to_not be_valid
+  end
+
+  it "is invalid without a last name" do
+    invalid_profile = FactoryGirl.build(:profile, last_name: nil)
+    expect(invalid_profile).to_not be_valid
+  end
+
+  it "is invalid without a country" do
+    invalid_profile = FactoryGirl.build(:profile, country: nil)
+    expect(invalid_profile).to_not be_valid
+  end
+
+  it "is invalid without an integer age" do
+    invalid_profile = FactoryGirl.build(:profile, age: 1.2)
+    expect(invalid_profile).to_not be_valid
+  end
+
+  it "is invalid without an acceptable gender" do
+    invalid_profile = FactoryGirl.build(:profile, gender: "foo")
+    expect(invalid_profile).to_not be_valid
+  end
 end
