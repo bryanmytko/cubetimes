3788192e948dcfd34913b1b84e1947d6d3a7b5ba
bryanmytko <bryanmytko@gmail.com>
2016-07-20 14:35:20 -0400

    Fix formatting

--- app/assets/javascripts/scramble.js
+++ app/assets/javascripts/scramble.js
@@ -1,10 +1,10 @@
 var Scramble = {
-  ALLOWED_MOVES_LENGTH : 12,
-  ALLOWED_TURNS_LENGTH : 3,
+  ALLOWED_MOVES_LENGTH: 12,
+  ALLOWED_TURNS_LENGTH: 3,
 
-  faces : ['u','f','d','l','r','b'],
+  faces: ['u','f','d','l','r','b'],
 
-  moveArray : {
+  moveArray: {
     'u': ['U','U\'','U2'],
     'f': ['F','F\'','F2'],
     'd': ['D','D\'','D2'],
@@ -17,7 +17,7 @@ var Scramble = {
    * moveArray. I.e, 'u' face inverse is 'd', so allow all values from
    * moveArray except self & inverse. This would make expanding to larger cubes
    * much easier by not requiring nextAllowedMove to be stricly defined. */
-  nextAllowedMove : {
+  nextAllowedMove: {
     'u': ['F','F\'','F2','B','B\'','B2','L','L\'','L2','R','R\'','R2'],
     'f': ['L','L\'','L2','R','R\'','R2','U','U\'','U2','D','D\'','D2'],
     'd': ['F','F\'','F2','B','B\'','B2','L','L\'','L2','R','R\'','R2'],
@@ -26,20 +26,20 @@ var Scramble = {
     'b': ['L','L\'','L2','R','R\'','R2','U','U\'','U2','D','D\'','D2'],
   },
 
-  randint : function(a, b) {
+  randint: function(a, b){
     var lower = Math.min(a, b);
     var upper = Math.max(a, b);
     var diff = upper - lower;
     return Math.floor((Math.random() * (diff + 1)) + lower);
   },
 
-  random_move : function() {
+  random_move: function(){
     var random_number = this.randint(0, this.ALLOWED_TURNS_LENGTH - 1);
     var random_face = this.faces[this.randint(0, this.faces.length - 1)];
     return this.moveArray[random_face][random_number];
   },
 
-  random_nextAllowedMove : function(current_move) {
+  random_nextAllowedMove: function(current_move){
     var random_number = this.randint(0, this.ALLOWED_MOVES_LENGTH - 1)
     var move_type;
 
@@ -52,7 +52,7 @@ var Scramble = {
     return this.nextAllowedMove[move_type][random_number];
   },
 
-  get_random_moves : function (requested_moves) {
+  get_random_moves: function(requested_moves){
     var move_count = requested_moves;
     var current_move = this.random_move();
     var moves = [];
@@ -61,6 +61,7 @@ var Scramble = {
        moves.push(current_move + ' ');
        current_move = this.random_nextAllowedMove(current_move);
     }
+
     return moves;
   }
 }
