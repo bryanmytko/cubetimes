1ae0c9ca721282e29db8bb8001eec163b18b6510
Bryan Mytko <bryanmytko@gmail.com>
2017-03-25 19:51:39 -0400

    Add dependent destroy for Solves -> CubingSessions

--- app/models/cubing_session.rb
+++ app/models/cubing_session.rb
@@ -2,16 +2,23 @@ class CubingSession < ActiveRecord::Base
   include JnetImport
 
   belongs_to :user
-  has_many :solves
-
-  validates :times, length: { is: 12 }
+  has_many :solves, dependent: :destroy, class_name: "Solve"
 
   def self.jnet_import(user, params)
-    create(
+    cubing_session = create(
       user: user,
-      times: JnetImport::extract_times(params),
       puzzle_type: "3x3",
       created_at: JnetImport::extract_date(params)
     )
+
+    # Check params here
+    debugger
+    # session_data_params[:session_array].each_pair do |index, value|
+    #   Solve.create(
+    #     time: params[:time],
+    #     scramble: params[:scramble],
+    #     cubing_session: cubing_session
+    #   )
+    # end
   end
 end
