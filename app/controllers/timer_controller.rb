38158478f584e21cbad1345a5a6bad9b57227958
bryanmytko <bryanmytko@gmail.com>
2017-03-25 18:29:16 -0400

    Begin adding functionality for parsing out the individual time data for a session and saving it in related Solves

--- app/controllers/timer_controller.rb
+++ app/controllers/timer_controller.rb
@@ -5,13 +5,30 @@ class TimerController < ApplicationController
   end
 
   def create
-    CubingSession.create(
-      user_id: current_user.id,
-      origin: "web",
-      times: params[:times],
-      puzzle_type: params[:puzzle_type]
-    )
+    if current_user
+      cubing_session = CubingSession.create(
+        user_id: current_user.id,
+        origin: "web",
+        puzzle_type: params[:puzzle_type]
+      )
+
+     # @TODO Finish this. The array is being converted to some kind of object,
+     #  which must use ActiveParameter methods to parse.
+      session_data_params[:session_array].each_pair do |index, value|
+        Solve.create(
+          time: solve.time,
+          scramble: value[:scramble},
+          cubing_session: value[:cubing_session]
+        )
+      end
+    end
 
     head :ok, content_type: "text/html"
+  end
+
+  private
+
+  def session_data_params
+    params.require(:session_data).permit(session_array: [:time, :scramble])
   end
 end
---
+++ test/factories/solves.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :solf, class: 'Solve' do
+    time "MyText"
+    cubing_session nil
+  end
+end
