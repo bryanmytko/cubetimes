7596c41886949d1fb7a1b3972fbc30cd2f9f5825
bryanmytko <bryanmytko@gmail.com>
2017-03-27 15:32:07 -0400

    Update stats helper methods to use Solve data instead of session times

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,23 +1,19 @@
 module StatisticsHelper
   def display_row(cubing_session)
-    best_found = false
-
     string =  "<td>#{date_format(cubing_session.created_at)}</td>"
     string += "<td class=\"average\">" \
       "#{session_average(cubing_session.times)}</td>"
 
-    # @TODO Change this and all other loops to use the CubingSession.solves times instead
-    cubing_session.solves.each do |time|
-      if time == cubing_session.times.min && !best_found
+    times = cubing_session.solves.pluck(:time).collect(&:to_i)
+    times.each do |time|
+      if(time == times.min)
         string += "<td class=\"best\">#{time}</td>"
-        best_found = true
       else
         string += "<td>#{time}</td>"
       end
     end
 
     string += destroy_row(cubing_session.id)
-
     raw string
   end
 
@@ -31,19 +27,19 @@ module StatisticsHelper
   end
 
   def best_time(cubing_sessions)
-    lowest_times = Array.new
-    cubing_sessions.each do |cubing_session|
-      lowest_times << cubing_session.times.min
-    end
-    lowest_times.min
+    cubing_sessions
+      .includes(:solves)
+      .pluck(:time)
+      .map(&:to_f)
+      .min
   end
 
   def best_average(cubing_sessions)
-    lowest_average = Array.new
-    cubing_sessions.each do |cubing_session|
-      lowest_average << session_average(cubing_session.times)
-    end
-    lowest_average.min
+    debugger
+    avgs = cubing_sessions
+      .includes(:solves)
+      .map { |session| session_average(session.solves.pluck(:time)) }
+      .min
   end
 
   private
