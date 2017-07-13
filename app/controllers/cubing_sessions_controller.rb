8972c1439d675166a7f45eccd8cfe7c144496fd2
bryanmytko <bryanmytko@gmail.com>
2017-07-13 19:56:40 -0400

    Refactor Jnet cube importing

--- app/controllers/cubing_sessions_controller.rb
+++ app/controllers/cubing_sessions_controller.rb
@@ -1,52 +1,34 @@
 class CubingSessionsController < ApplicationController
-  def create
-  end
+  # before_action :validate_file_format, only: [:create]
 
-  def jnet_import
-    if params[:cubing_session] && upload_valid?
-      import_jnet_data
+  def create
+    if valid_file_format?
+      cubing_session = CubingSession.create(
+        user: current_user,
+        puzzle_type: "3x3",
+        created_at: JnetImport::extract_date(session_file)
+      )
+
+      CubingSession.import(cubing_session, session_file)
+      flash[:alert] = "Thank you, your session has been uploaded!"
     else
-      upload_error "Sorry, you can only upload text files!"
+      flash[:notice] = "Sorry, that file could not be uploaded"
     end
+
+    redirect_to statistics_path
   end
 
   private
 
-  def import_jnet_data
-    if cubing_session.errors.blank?
-      upload_success
-    else
-      upload_error "Sorry, that data could not be uploaded!"
-    end
-  end
-
-  def upload_valid?
-    whitelist.include? session_file.content_type
-  end
-
   def whitelist
     %w(text/plain)
-  end
-
-  def current_user_cubing_session
-    current_user.cubing_session
   end
 
   def session_file
     params[:cubing_session][:session_file]
   end
 
-  def cubing_session
-    current_user_cubing_session.jnet_import(current_user, session_file)
-  end
-
-  def upload_success
-    flash[:alert] = "Thank you, your session has been uploaded!"
-    redirect_to statistics_path
-  end
-
-  def upload_error(msg)
-    flash[:notice] = msg
-    redirect_to statistics_path
+  def valid_file_format?
+    whitelist.include? session_file.content_type
   end
 end
