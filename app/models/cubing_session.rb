12b798fb491de4a7dcefc0669a40790b9cda27ec
Bryan Mytko <bryanmytko@gmail.com>
2017-07-14 21:33:14 -0400

    Refactor JnetImport to extract solve data
    Previously there were two methods to extract time and scramble data,
    which was then zipped together. This version instead extracts the data
    directly and uses that to create the Solve objects.

--- app/models/cubing_session.rb
+++ app/models/cubing_session.rb
@@ -5,15 +5,13 @@ class CubingSession < ActiveRecord::Base
   has_many :solves, dependent: :destroy, class_name: "Solve"
 
   def self.import(cubing_session, file)
-    times = JnetImport::extract_times(file)
-    scrambles = JnetImport::extract_scrambles(file)
+    solves = JnetImport::extract_solves(file)
 
-    times
-      .zip(scrambles)
+    solves
       .each do |solve|
         Solve.create(
-          time: solve[0],
-          scramble: solve[1],
+          time: solve[:time],
+          scramble: solve[:scramble],
           cubing_session: cubing_session
         )
       end
