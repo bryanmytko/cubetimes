a56a805a777119da0331e4dcb55fdd04ea03f872
Bryan Mytko <bryanmytko@gmail.com>
2022-01-03 07:01:53 -0500

    Update Rails 6.0.4.4; security issue & fix localhost bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Cubetimes::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.hosts << "localhost"
+  config.hosts << "0.0.0.0"
 end
